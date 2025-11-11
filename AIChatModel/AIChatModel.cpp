b68bfab0740786bfd06666bcab231001e73dc0f0
inesacosic <7inesacosic7@gmail.com>
2025-11-10 19:38:57 -0500

    Added another function: printChatHistory()

--- AIChatModel/AIChatModel.cpp
+++ AIChatModel/AIChatModel.cpp
@@ -7,21 +7,67 @@ using namespace std;
 
 class AIChatModel{
     private:
-        string model_name = "llama3.2:3b";
+        string model_name = "llama3.2:3b"; // ollama model we are using is llama3.2:3b
         ollama::messages chat_history; // initializes a vector where we will push all messages to build context
 
     public:
         string generateResponse(string input);
+        void printChatHistory();
 
 };
 
+
+
+/*
+# generateResponse(string input)
+#
+# This function adds the user's input into the AIChatModel's chat_history. It then generates a 
+# response from the ollama model. Finally, it adds the model's response to the chat_history.
+# Inputs and outputs are added to the AIChatModel's chat_history to build context or "memory".
+#
+# Parameters:
+#   1. input -> a string value which is the input given by the user 
+#
+# Return Type:
+#   string - > returns the response from the ollama model
+#
+*/
 string AIChatModel::generateResponse(string input){
     
+    // create a message from the user's input and add it to this -> chat_history
+    ollama::message user_input("user", input);
+    this -> chat_history.push_back(user_input);
+
     cout << "Connecting to ollama..." <<endl;
-    string reply = ollama::generate(this->model_name, input);
-    ollama::message response("assistant", reply);
+
+    // generate a response from the chat history and create a new message with the response
+    string ollama_reply = ollama::chat(this->model_name, chat_history);
+    ollama::message response("assistant", ollama_reply);
+
+    // add the new message to this -> chat_history
     this -> chat_history.push_back(response);
-    return reply;
+
+    return ollama_reply;
+}
+
+
+
+/* printChatHistory()
+#
+# This function prints the AIChatModel's chat_history
+#
+# Return Type:
+#   void
+#
+*/
+void AIChatModel::printChatHistory(){
+
+    cout << "-----------CHAT HISTORY--------------" << endl;
+    for(const auto& msg: chat_history){
+        // convert message to JSON
+        cout << msg << endl;
+    }
+
 }
 
 int main(){
@@ -32,6 +78,7 @@ int main(){
     string response = ai.generateResponse(input);
 
     cout << "Model's response: " << response << endl;
+
 	return 0;
 
 }
